e83f1031ef09fadfd4c77651c7bf9c54123fbe5f
Simon Hylleberg <simonhylleberg@gmail.com>
2019-03-08 19:57:00 +0100

    Add project files.

---
+++ PaddeWeb/PaddeWeb/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for PaddeWeb.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', PaddeWeb.views.home, name='home'),
+    # url(r'^PaddeWeb/', include('PaddeWeb.PaddeWeb.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
